53a0bad392b5ecf7d85fdfde8faa5b12dc7cc949
Patricio Bertini <patobertini10@gmail.com>
2022-08-10 16:44:44 +0200

    cambiando descripcion de inicio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto de Patito.
         </p>
         <a
           className="App-link"
